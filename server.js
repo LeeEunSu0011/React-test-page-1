e6409ec9e9ad27490622eb9b2f42da84a7f76e0c
okg001 <okg001@user-no-MacBook-Air.local>
2020-11-16 11:22:16 +0900

    create customer Database table Node.js databse connection setting

--- server.js
+++ server.js
@@ -2,37 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.port || 5000;
+const fs = require('fs');
+const { request } = require('http');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql'); 
+
+const connection = mysql.createConnection({
+    host : conf.host,
+    user : conf.user,
+    password : conf.password,
+    port : conf.port,
+    database : conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res )  => {
-    res.send(
-        [{
-            'id' : 1,
-            'image' : 'https://placeimg.com/64/64/1',
-            'name' : 'cccc',
-            'birthday' : '99 99 99',
-            'gender' : '男',
-            'job' : 'programer'
-        },
-        {
-            'id' : 2,
-            'image' : 'https://placeimg.com/64/64/2',
-            'name' : 'dddd',
-            'birthday' : '88 88 99',
-            'gender' : '女',
-            'job' : 'programer'
-        },
-        {
-            'id' : 3,
-            'image' : 'https://placeimg.com/64/64/3',
-            'name' : 'aaaa',
-            'birthday' : '77 99 99',
-            'gender' : '男',
-            'job' : 'programer'
+    connection.query(
+        "select * from customer;",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-    ]);
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
